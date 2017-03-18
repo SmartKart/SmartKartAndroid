aa78c0daa43669ab7e07e3120e791e1720a538a4
eyesniper2 <eyesniper2@rayfall.net>
2017-03-18 13:22:39 -0700

    Base Commit

---
+++ app/src/main/java/com/getsmartkart/smartkart/ActiveShoppingCartActivity.java
@@ -0,0 +1,13 @@
+package com.getsmartkart.smartkart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActiveShoppingCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_shopping_cart);
+    }
+}
